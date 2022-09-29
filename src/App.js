c53ab291b33ffbb0d6d7a707132961918701ac9b
JanainaNogueira <janainanogueira464@gmail.com>
2022-09-29 15:14:31 -0300

    Add authorization page import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import Style from './App.module.css'
 import Register from './Components/pages/Register'
 import Login from './Components/pages/Login'
-import Style from './App.module.css'
+import Authorization from './Components/pages/authorization';
 
 function App() {
   return (
     <div className={Style.container_App}>
-      <Register/>
+      <Authorization/>
     </div>
   );
 }
