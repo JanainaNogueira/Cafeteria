ee1b4b4b9ad2458f6aa95bfbbec08e332d423ff5
JanainaNogueira <janainanogueira464@gmail.com>
2022-10-06 19:55:19 -0300

    Import order component

--- src/Components/pages/Home/Home.js
+++ src/Components/pages/Home/Home.js
@@ -1,10 +1,16 @@
-import Header from '../../Layouts/Header'
+import Header from '../../Layouts/Header/Header'
+import BoxOrder from '../../Layouts/boxOrder'
 import Style from './Home.module.css'
 
 function Home(){
     return(
         <div className={Style.container}>
             <Header/>
+            <div className={Style.main}>
+                <div className={Style.tables}>
+                    <BoxOrder/>
+                </div>
+            </div>
         </div>
     )
 }
