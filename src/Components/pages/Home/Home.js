f3da3c04e615399e367f5a4261762ffd064cb32a
JanainaNogueira <janainanogueira464@gmail.com>
2022-10-07 01:04:49 -0300

    Add  Bootstrap icon plus by react icon

--- src/Components/pages/Home/Home.js
+++ src/Components/pages/Home/Home.js
@@ -1,3 +1,4 @@
+import { BsPlusSquare } from "react-icons/bs";
 import Header from '../../Layouts/Header/Header'
 import BoxOrder from '../../Layouts/boxOrder'
 import Style from './Home.module.css'
@@ -7,6 +8,9 @@ function Home(){
         <div className={Style.container}>
             <Header/>
             <div className={Style.main}>
+                <div className={Style.icon}>
+                    <BsPlusSquare className={Style.iconPlus}/>
+                </div>
                 <div className={Style.tables}>
                     <BoxOrder/>
                 </div>
