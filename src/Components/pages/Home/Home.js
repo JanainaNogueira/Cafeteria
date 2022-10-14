f6e42b119ff0e1edabe762ed005e9436454a03d7
JanainaNogueira <janainanogueira464@gmail.com>
2022-10-13 21:49:57 -0300

    Functionality: Create modal to add table number and time to start order

--- src/Components/pages/Home/Home.js
+++ src/Components/pages/Home/Home.js
@@ -1,21 +1,55 @@
 import { FiPlusSquare } from "react-icons/fi";
-import {Link} from "react-router-dom"
+import { useState } from "react";
+import { Link } from "react-router-dom"
 import Header from '../../Layouts/Header/Header'
 import BoxOrder from '../../Layouts/boxOrder'
 import Style from './Home.module.css'
+import Styled from "../../ModalTab.module.css"
+
 
 function Home(){
+const [showModal, setShowModal]= useState(false);
     return(
-        <div className={Style.container}>
-            <Header/>
-            <main className={Style.main}>
-                <Link to="/dishtypes" className={Style.icon}>
-                    <FiPlusSquare className={Style.iconPlus}/>
-                </Link>
-                <div className={Style.tables}>
-                    <BoxOrder/>
+        <div className={Styled.container}>
+            {showModal?
+            (
+            <div className={Styled.modal}>
+                <div className={Styled.containe}>
+                    <div className={Styled.numberTable}>
+                        <h4>Número da mesa</h4>
+                    </div>
+                    <div className={Styled.infoTable}>
+                        <input type='text' placeholder="Digite o número da mesa"/>
+                        <p>12h00</p>
+                    </div>
+                    <div className={Styled.buttonModal}>
+                        <button className={Styled.button} onClick={()=>{
+                            setShowModal(false)
+                        }}
+                        >
+                            Voltar
+                        </button>
+                        <Link className={Styled.button} to='/dishtypes'>Adicionar Prato</Link>
+                    </div>
                 </div>
-            </main>
+            </div>
+            ):(
+            <>
+            </>
+            )}
+            <Header/>
+                <main className={Style.main}>
+                    <div className={Style.icon}>
+                        <button onClick={()=>{
+                            setShowModal(true)
+                        }}>
+                            <FiPlusSquare className={Style.iconPlus}/>
+                        </button>
+                    </div>
+                    <div className={Style.tables}>
+                        <BoxOrder/>
+                    </div>
+                </main>
         </div>
     )
 }
